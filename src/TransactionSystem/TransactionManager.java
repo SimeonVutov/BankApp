f4351573d80d36692b64f52410d5d3a7fb955c3f
Simeon32 <simeon32.dev@gmail.com>
2022-12-24 19:39:52 +0200

    Now the TransactionManager class has a list which contains all transactions. When the Manager is created, we can pass a list of transactions which it will store and work with.

--- src/TransactionSystem/TransactionManager.java
+++ src/TransactionSystem/TransactionManager.java
@@ -7,21 +7,37 @@ package TransactionSystem;
 import bankapp.BankAccount;
 import java.math.BigDecimal;
 import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
  * @author Simeon_32
  */
 public class TransactionManager {
+    private List<Transaction> _transactions;
     private TransactionProcessor _transactionProcessor;
     
     public TransactionManager() {
+        _transactions = new LinkedList<Transaction>();
         _transactionProcessor = new TransactionProcessor(Duration.ofSeconds(5));
+        _transactionProcessor.Start();
+    }
+    
+    public TransactionManager(List<Transaction> transactions) {
+        _transactions = transactions;
+        _transactionProcessor = new TransactionProcessor(Duration.ofSeconds(5));
+        for (Transaction transaction : transactions) {
+            if(transaction.getStatus() == TransactionStatus.Pending) {
+                _transactionProcessor.AddTransactionToQueue(transaction);
+            }
+        }
         _transactionProcessor.Start();
     }
     
     public void CreateTransaction(BigDecimal money, BankAccount fromBankAccount, BankAccount toBankAccount) {
         Transaction newTransaction = new Transaction(money, fromBankAccount, toBankAccount);
+        _transactions.add(newTransaction);
         // TODO: Save the transaction to the database if its creation is successful
         _transactionProcessor.AddTransactionToQueue(newTransaction);
     }
